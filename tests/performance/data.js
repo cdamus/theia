f71cb4e947c705066e61972748538ee94b66a27c
github-action-benchmark <github@users.noreply.github.com>
2021-11-12 17:06:06 +0000

    add Performance Benchmarks (customSmallerIsBetter) benchmark result for a22777e7988043a1dfe4772ba3333531ffab1fa1

--- tests/performance/data.js
+++ tests/performance/data.js
@@ -1,8 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636735391254,
+  "lastUpdate": 1636736766235,
   "repoUrl": "https://github.com/cdamus/theia",
   "entries": {
-    "Benchmark": [
+    "Performance Benchmarks": [
       {
         "commit": {
           "author": {
@@ -32,6 +32,42 @@ window.BENCHMARK_DATA = {
             "value": 5.325575300000001,
             "unit": "seconds",
             "range": 0.1147149559735345
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "give.a.damus@gmail.com",
+            "name": "Christian W. Damus",
+            "username": "cdamus"
+          },
+          "committer": {
+            "email": "give.a.damus@gmail.com",
+            "name": "Christian W. Damus",
+            "username": "cdamus"
+          },
+          "distinct": true,
+          "id": "a22777e7988043a1dfe4772ba3333531ffab1fa1",
+          "message": "Perf history and publication\n\n- track performance history\n- use benchmark action to check for regression\n- publish results to GH Pages\n\nContributed on behalf of STMicroelectronics.\n\nSigned-off-by: Christian W. Damus <give.a.damus@gmail.com>",
+          "timestamp": "2021-11-12T11:57:22-05:00",
+          "tree_id": "30e47a7ba44c4db170505e011e85ef9eb22f3e9a",
+          "url": "https://github.com/cdamus/theia/commit/a22777e7988043a1dfe4772ba3333531ffab1fa1"
+        },
+        "date": 1636736762361,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Browser Frontend Startup",
+            "value": 4.9853698,
+            "unit": "seconds",
+            "range": 0.46978116717186524
+          },
+          {
+            "name": "Electron Frontend Startup",
+            "value": 6.2883984,
+            "unit": "seconds",
+            "range": 0.2928329189726455
           }
         ]
       }
