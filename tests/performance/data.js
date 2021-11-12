eecae1a9aaf4ccdae74f02daa97b352873f2f892
github-action-benchmark <github@users.noreply.github.com>
2021-11-12 16:43:11 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c245ab8381928e0cc70d48be52f68e4c7ab40111

---
+++ tests/performance/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1636735391254,
+  "repoUrl": "https://github.com/cdamus/theia",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "cdamus",
+            "username": "cdamus"
+          },
+          "committer": {
+            "name": "cdamus",
+            "username": "cdamus"
+          },
+          "id": "c245ab8381928e0cc70d48be52f68e4c7ab40111",
+          "message": "WIP: Integrate performance tests into the build",
+          "timestamp": "2021-11-05T20:06:56Z",
+          "url": "https://github.com/cdamus/theia/pull/1/commits/c245ab8381928e0cc70d48be52f68e4c7ab40111"
+        },
+        "date": 1636735387699,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Browser Frontend Startup",
+            "value": 3.9051314999999995,
+            "unit": "seconds",
+            "range": 0.1688827035970529
+          },
+          {
+            "name": "Electron Frontend Startup",
+            "value": 5.325575300000001,
+            "unit": "seconds",
+            "range": 0.1147149559735345
+          }
+        ]
+      }
+    ]
+  }
+}
